07f55a38b440d36d1e523a824839ffcce814b7a1
truongphan4898 <truongphan4898@gmail.com>
2024-05-30 12:02:28 +0700

    add

---
+++ src/main/java/com/th_security/security/SecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.th_security.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
